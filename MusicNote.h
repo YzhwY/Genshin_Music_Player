9038dbf60574aceef06b7e972c1b72504b1fcc74
YzwhY <121091083+YzhwY@users.noreply.github.com>
2023-09-23 09:48:26 +0800

    现在可以用ASD代表do re mi，省去了很长的一串if
    定义头文件

---
+++ MusicNote.h
@@ -0,0 +1,32 @@
+#ifndef HEADERDISPOSE_H
+#define HEADERDISPOSE_H
+
+//用这个头文件记录每个音符所对应的字母，缩减代码长度，
+
+//C3
+#define Z 1
+#define X 2
+#define C 3
+#define V 4
+#define B 5
+#define N 6
+#define M 7
+
+//C4
+#define A 8
+#define S 9
+#define D 10
+#define F 11
+#define G 12
+#define H 13
+#define J 14
+//C5
+#define Q 15
+#define W 16
+#define E 17
+#define R 18
+#define T 19
+#define Y 20
+#define U 21
+
+#endif
--- play_sound.cpp
+++ play_sound.cpp
@@ -1,8 +1,9 @@
-#include <iostream>
+/*#include <iostream>
 #include <Windows.h>
 #include <map>
 #include <thread>
-#include<vector>
+#include <vector>
+#include <queue>
 
 #pragma comment(lib, "winmm.lib")
 
@@ -53,6 +54,16 @@ void play_sound(int noteValue, int duration) {
     midiOutClose(handle);
 }
 
+void play_sound_chord(queue<int>& chordQueue, queue<int>& durationQueue) {
+    while (!chordQueue.empty()) {
+        int noteValue = chordQueue.front();
+        int noteDuration = durationQueue.front();
+        chordQueue.pop();
+        durationQueue.pop();
+        play_sound(noteValue, noteDuration);
+    }
+}
+
 void p(string name,int time) {
     if (name == "A") {
         play_sound(8, time);
@@ -81,6 +92,112 @@ int main() {
     p("A", 500);
     p("S", 500);
     p("D", 500);
+    queue<int> chordQueue;
+    queue<int> durationQueue;
+    // 向队列添加音符和持续时间
+    chordQueue.push(8); // 8代表do
+    chordQueue.push(9); // 9代表re
+    durationQueue.push(500); // 持续时间
+    durationQueue.push(500);
+    thread th1(play_sound_chord, ref(chordQueue), ref(durationQueue));
+    th1.join(); // 等待线程结束
+    return 0;
+}
+
+*/
+#include <iostream>
+#include <Windows.h>
+#include <map>
+#include <thread>
+#include <vector>
+#include <queue>
+#include "MusicNote.h"
+
+#pragma comment(lib, "winmm.lib")
+
+using namespace std;
+
+//显而易见，下面这个map是ChatGPT编写的
+map<int, int> noteToMidi = {
+    {1, 46}, //do
+    {2, 48}, //re
+    {3, 50}, //mi
+    {4, 52}, //fa
+    {5, 54}, //so
+    {6, 56}, //la
+    {7, 58},//ti
+    {8, 60},//do
+    {9, 62},//re
+    {10, 64},//mi
+    {11, 66},//fa
+    {12, 68},//so
+    {13, 70},//la
+    {14, 72},//ti
+    {15, 74},//do
+    {16, 76},//re
+    {17, 78},//mi
+    {18, 80},//fa
+    {19, 82},//so
+    {20, 84},//la
+    {21, 86},//ti
+    {22, 88},//do
+};
+
+//显而易见，下面这个函数是ChatGPT编写的
+void play_sound(int noteValue, int duration) {
+    HMIDIOUT handle;
+    midiOutOpen(&handle, 0, 0, 0, CALLBACK_NULL);
+
+    int volume = 0x7F; // 音量（0-127）
+
+    int midiValue = noteToMidi[noteValue];
+    int midiMessage = (volume << 16) + (midiValue << 8) + 0x90; // MIDI Note On 消息
+
+    midiOutShortMsg(handle, midiMessage);
+
+    Sleep(duration);
+
+    midiOutShortMsg(handle, (volume << 16) + (midiValue << 8) + 0x80);
+
+    midiOutClose(handle);
+}
+
+void play_sound_chord(queue<int>& chordQueue, queue<int>& durationQueue) {
+    while (!chordQueue.empty()) {
+        int noteValue = chordQueue.front();
+        int noteDuration = durationQueue.front();
+        chordQueue.pop();
+        durationQueue.pop();
+        play_sound(noteValue, noteDuration);
+    }
+}
+
+/*void p(string name,int time) {
+    if (name == "A") {
+        play_sound(8, time);
+    }
+    if (name == "S") {
+        play_sound(9, time);
+    }
+    if (name == "D") {
+        play_sound(10, time);
+    }
+    if (name == "F") {
+        play_sound(11, time);
+    }
+    if (name == "G") {
+        play_sound(12, time);
+    }
+    if (name == "H") {
+        play_sound(13, time);
+    }
+    if (name == "J") {
+        play_sound(14, time);
+    }
+}*/
+
+int main() {
+    play_sound(A, 500);
     return 0;
 }
 
